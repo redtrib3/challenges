c66316b85bcbc3be857f678ae736c6cb86b1981b
redtrib3 <anirudhkp2004@gmail.com>
2023-10-07 23:13:44 +0530

    new OSINT challenge: seabound

--- js/submit.js
+++ js/submit.js
@@ -33,7 +33,7 @@ function submitflag(challengeId)
     thisbutton.classList.add("is-loading");
     
     const startChal = 1330;
-    const endChal = 1344;
+    const endChal = 1345;
     const ids = Object.fromEntries(Array.from({ length: endChal - startChal + 1 }, (_, i) => [startChal + i, false]));
 
     localStorage.setItem("ids",JSON.stringify(ids)); 
