e09c5dcd3a3a1931c57dbd068c561f33f04bcb7d
redtrib3 <anirudhkp2004@gmail.com>
2024-03-21 21:03:13 +0530

    New Rev challenge: Secure-Vault

--- js/submit.js
+++ js/submit.js
@@ -33,7 +33,7 @@ function submitflag(challengeId)
     thisbutton.classList.add("is-loading");
     
     const startChal = 1330;
-    const endChal = 1350;  //dyno
+    const endChal = 1351;  //dyno
     const ids = Object.fromEntries(Array.from({ length: endChal - startChal + 1 }, (_, i) => [startChal + i, false]));
 
     localStorage.setItem("ids",JSON.stringify(ids)); 
